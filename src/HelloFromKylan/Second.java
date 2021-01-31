6a7d970ea3859eec7332e62bff198e5ac3e97694
Timothy Eatough <u517514543@byui.edu>
2021-01-30 19:57:33 -0700

    Added my name

--- src/HelloFromKylan/Second.java
+++ src/HelloFromKylan/Second.java
@@ -3,6 +3,9 @@ package HelloFromKylan;
 public class Main {
 
         public static void main(String[] args) {
+
             System.out.println("Hello! My name is Kylan.");
+
+            System.out.println("Hello Kylan! My name is Timothy.");
         }
 }
